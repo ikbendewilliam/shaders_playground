3000aae7418d2b26a1e0094104e6929d1ca65fe9
WilliamVerhaeghe <william.verhaeghe@icapps.com>
2023-07-17 10:58:02 +0200

    Always render with canvaskit

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "eb4812f2131e71135f7c133fad95dbc6",
-"index.html": "0b6d831a205cb8113c919e2b3789730b",
-"/": "0b6d831a205cb8113c919e2b3789730b",
-"main.dart.js": "079c14ea17fb884fefbbac8c41336850",
+"index.html": "92ed486013bd9acdfaa8504f4b339c59",
+"/": "92ed486013bd9acdfaa8504f4b339c59",
+"main.dart.js": "96bcd04bb3d14446201b6526e414a62f",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
